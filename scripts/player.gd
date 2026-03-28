3e7b9cef41b065b82c1bfbbe6e5d8ad6895f9a09
miyu210429 <k.miyu210429@gmail.com>
2026-03-28 12:00:29 +0900

    マップに新しいシーンを追加しました。 新しいシーンのマップを作成し、シーンの切り替えができるようにしました。 新しいシーンの方でも、プレイヤーを追従させるようなカメラを設定しました

--- scripts/player.gd
+++ scripts/player.gd
@@ -19,6 +19,7 @@ func _physics_process(delta):
 	player_movement(delta)
 	enemy_attack()
 	attack()
+	current_camera()
 	
 	if health <= 0:
 		player_alive = false
@@ -143,3 +144,13 @@ func _on_deal_attack_timer_timeout() -> void:
 	$deal_attack_timer.stop()
 	global.player_current_attack = false
 	attack_ip = false
+
+
+func current_camera():
+	if global.current_scene == "world":
+		$world_camera.enabled = true
+		$cliffside_camera.enabled = false
+	elif global.current_scene == "cliff_side":
+		$world_camera.enabled = false
+		$cliffside_camera.enabled = true
+		
